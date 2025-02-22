6560bd06716ce9bd3de4f46c8a79bd341126cb61
SancheSLozovoy <alex.lozovoy05@bk.ru>
2025-02-23 00:12:36 +0300

    init project without next

--- client/src/app/register/page.tsx
+++
@@ -1,5 +0,0 @@
-import styles from "@/app/register/register.module.scss";
-
-export default function Page() {
-  return <h1>Регистрация</h1>;
-}
---
+++ client/src/pages/register/index.tsx
@@ -0,0 +1,7 @@
+import FormRegister from "../../components/Form.tsx";
+
+function Register() {
+  return <FormRegister />;
+}
+
+export default Register;
