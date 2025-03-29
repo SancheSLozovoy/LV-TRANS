27c85864db26fc3a54401b82ee99337f59014671
SancheSLozovoy <alex.lozovoy05@bk.ru>
2025-03-29 13:06:54 +0300

    update: updated styles

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,18 +1,23 @@
 import styles from "./footer.module.scss";
+import { Container } from "../container/Container.tsx";
 
 export const Footer = () => {
   return (
     <div className={styles.footer}>
       <div className={styles.footer__inner}>
-        <div className={styles.footer__contacts}>
-          <a href="mailto:info@lv-trans.ru">
-            <div>info@lv-trans.ru</div>
-          </a>
-          <a href="tel:+79281346454">
-            <div>+7 (928) 134-64-54</div>
-          </a>
-        </div>
-        <div className={styles.footer__year}>© 2025 LV-Trans</div>
+        <Container>
+          <div className={styles.footer__content}>
+            <div className={styles.footer__contacts}>
+              <a href="mailto:info@lv-trans.ru">
+                <div>info@lv-trans.ru</div>
+              </a>
+              <a href="tel:+79281346454">
+                <div>+7 (928) 134-64-54</div>
+              </a>
+            </div>
+            <div className={styles.footer__year}>© 2025 LV-Trans</div>
+          </div>
+        </Container>
       </div>
     </div>
   );
--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -3,6 +3,7 @@ import Logo from "../../assets/images/logo.png";
 import Profile from "../../assets/images/profile.svg";
 import { Link } from "react-router-dom";
 import styles from "./header.module.scss";
+import { Container } from "../container/Container.tsx";
 
 const scrollToSection = (id: string) => {
   const element = document.getElementById(id);
@@ -15,22 +16,28 @@ export const Header = (): React.JSX.Element => {
   return (
     <header className={styles.header}>
       <div className={styles.header__container}>
-        <div className={styles.header__logo}>
-          <img src={Logo} alt="Logo" />
-        </div>
-        <nav className={styles.header__nav}>
-          <ul className={styles.header__nav_list}>
-            <li onClick={() => scrollToSection("main")}>Главная</li>
-            <li onClick={() => scrollToSection("advantages")}>Преимущества</li>
-            <li onClick={() => scrollToSection("about")}>О нас</li>
-            <li onClick={() => scrollToSection("contact")}>Связь</li>
-          </ul>
-        </nav>
-        <div className={styles.header__profile}>
-          <Link to="/profile">
-            <img src={Profile} alt="Profile" />
-          </Link>
-        </div>
+        <Container>
+          <div className={styles.header__content}>
+            <div className={styles.header__logo}>
+              <img src={Logo} alt="Logo" />
+            </div>
+            <nav>
+              <ul className={styles.header__nav_list}>
+                <li onClick={() => scrollToSection("main")}>Главная</li>
+                <li onClick={() => scrollToSection("advantages")}>
+                  Преимущества
+                </li>
+                <li onClick={() => scrollToSection("about")}>О нас</li>
+                <li onClick={() => scrollToSection("contact")}>Связь</li>
+              </ul>
+            </nav>
+            <div>
+              <Link to="/profile">
+                <img src={Profile} alt="Profile" />
+              </Link>
+            </div>
+          </div>
+        </Container>
       </div>
     </header>
   );
