8db5218a4a5c18d0894cd4e19f92e0656f2a27b8
SancheSLozovoy <alex.lozovoy05@bk.ru>
2025-05-06 20:29:13 +0300

    fix: попытка починить уведомление на сервере

--- server/src/middleware/emailTransporter.js
+++ server/src/middleware/emailTransporter.js
@@ -3,9 +3,17 @@ import nodemailer from 'nodemailer';
 export const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
-    secure: false,
+    secure: process.env.SMTP_SECURE === 'true',
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
     },
+});
+
+transporter.verify((error) => {
+    if (error) {
+        console.error('SMTP error:', error);
+    } else {
+        console.log('SMTP подключение успешно!');
+    }
 });
